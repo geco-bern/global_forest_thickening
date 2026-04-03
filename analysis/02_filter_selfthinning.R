8607efc29f3fb4d2195fb8b48e45d421f6cc0be5
Beni Stocker <benjamin.stocker@gmail.com>
2026-04-03 23:01:48 +0200

    added slope filter simply based on quantile range

--- analysis/02_filter_selfthinning.R
+++ analysis/02_filter_selfthinning.R
@@ -18,6 +18,7 @@
 ## Loading ------------------------------------------------------------------
 library(tidyverse)
 library(here)
+library(segmented)
 
 source(here("R/identify_disturbed_plots.R"))
 source(here("R/identify_ingrowth_plots.R"))
@@ -117,3 +118,63 @@ ggsave(
   height = 10
 )
 
+### Filter based on STL slope ----------
+
+filter_stl_slope2 <- function(df){
+
+  df <- data_unm_und_noin_qmdfilt$data[[1]]
+
+  df_byplot <- df |>
+    group_by(plotID) |>
+    nest() |>
+    mutate(nobs = purrr::map_int(data, ~nrow(.))) |>
+    filter(nobs >= 3) |>  # minimum satisfied by design of data selection, but here for safety
+
+    # linear model
+    mutate(
+      linmod = purrr::map(data, ~lm(logDensity ~ logQMD, data = .))
+    ) |>
+    
+    # segmented model (1 breakpoint → 2 segments)
+    mutate(
+      segmod = map(linmod, ~ {
+        tryCatch(
+          segmented(.x, seg.Z = ~logQMD, npsi = 1),
+          error = function(e) NULL
+        )
+      })
+    ) |> 
+    
+    mutate(
+      slope_lin = map_dbl(linmod, ~ coef(.x)[2]),
+      slopes_seg = map(segmod, ~ if (is.null(.x)) NA_real_ else slope(.x)$logQMD_sc[, "Est."])
+    )
+  
+  # Retain only inner 90% of the data by slope from simple linear regression
+  range_accepted <- quantile(df_byplot$slope_lin, c(0.05, 0.95), na.rm = TRUE)
+
+  df_byplot |> 
+    ggplot(aes(slope_lin, after_stat(count))) +
+    geom_histogram(color = "black", fill = "grey70") +
+    xlim(range_accepted + c(-10, 10)) +
+    annotate(
+      "rect",
+      xmin = range_accepted[1], xmax = range_accepted[2],
+      ymin = 0, ymax = Inf,
+      fill = "red",
+      alpha = 0.3  # transparency
+    )
+
+  df <- df_byplot |> 
+    mutate(
+      bad_lin = slope_lin < range_accepted[1] | slope_lin > range_accepted[2],
+      bad_seg = map_lgl(slopes_seg, ~ any(.x < range_accepted[1] | .x > range_accepted[2], na.rm = TRUE))
+    ) |> 
+    mutate(
+      badslope = bad_lin | bad_seg
+    ) %>%
+    select(plotID, badslope, data) |> 
+    unnest(data)
+
+  return(df)
+}
